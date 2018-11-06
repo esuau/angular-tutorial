02e478a1e7a558ddf4abe441b02d7045bea6aadd
esuau <evan.suau@etu.u-pec.fr>
2018-11-06 23:48:49 +0100

    Add solution to step-01

---
+++ src/app/components/people/people.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.css']
+})
+export class PeopleComponent implements OnInit {
+
+  name = 'people works!';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
